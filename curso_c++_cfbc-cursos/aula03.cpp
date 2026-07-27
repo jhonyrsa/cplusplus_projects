ab81b818c8846e10f540a7196350dc64f44af663
Jhony Almeida <jhonyrsa@gmail.com>
2026-07-27 16:40:13 -0300

    Primeiro programa em C++

---
+++ curso_c++_cfbc-cursos/aula03.cpp
@@ -0,0 +1,19 @@
+//biblioteca padrão do c++
+#include <iostream>
+
+//indica qual espaço de trabalho ele vai buscar para executar, no caso na biblioteca standard (biblioteca padrão)
+using namespace std;
+
+int main(){
+	
+	//endl > quebra o final da linha
+	//cout > saída de texto na tela
+	// \n > quebra de linha
+	cout << "Hello World!" << endl;
+	
+	//para funções do tipo int é necessário retornar um valor para identificar que o programa acabou
+	// neste caso é 0!
+	
+	system("pause");
+	return 0;
+}
